0035a9f456ff26375e16db018a172cd7f199bd37
Chemistry101 <146695920+Chemistry101@users.noreply.github.com>
2023-10-02 14:24:39 +0530

    Project_77

---
+++ main.js
@@ -0,0 +1,22 @@
+a = 24;
+b = 4;
+w = 0;
+console.log("This is My first Console Project");
+console.log("I have done addition, subtraction, multiplication and division.");
+
+console.log(a +" + " +b);
+w = a + b;
+console.log(w);
+
+console.log(a +" - " +b);
+w = a - b;
+console.log(w);
+
+console.log(a +" x " +b);
+w = a * b;
+console.log(w);
+
+console.log(a +" / " +b);
+w = a / b;
+console.log(w);
+console.log("Thank you");
